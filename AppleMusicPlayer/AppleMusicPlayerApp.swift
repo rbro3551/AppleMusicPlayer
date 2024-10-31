14899fc26a14d03007369a119fa9d3c57a9e096c
Riley B <30447344+rbro3551@users.noreply.github.com>
2024-10-30 20:43:45 -0500

    Initial Commit

---
+++ AppleMusicPlayer/AppleMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicPlayerApp.swift
+//  AppleMusicPlayer
+//
+//  Created by Riley Brookins on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusicPlayerTests/AppleMusicPlayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicPlayerTests.swift
+//  AppleMusicPlayerTests
+//
+//  Created by Riley Brookins on 10/30/24.
+//
+
+import Testing
+@testable import AppleMusicPlayer
+
+struct AppleMusicPlayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
